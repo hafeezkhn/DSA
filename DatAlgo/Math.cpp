33ea765e92c1140fc27fa5e3d89f1e329d277f4a
hafeezkhn <hafeez017@outlook.com>
2023-05-06 16:26:40 +0100

    init

---
+++ DatAlgo/Math.cpp
@@ -0,0 +1,7 @@
+class Math{
+public:
+	int add(int a, int b){
+		return a+b;
+	}
+
+};
---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <gtest/gtest.h>
+#include "DatAlgo/Math.cpp"
+//#include "DatAlgo/whattotest.cpp"
+//TEST(test_case, example_test)
+//{
+//           EXPECT_EQ(2, 2);
+//}
+//TEST(SquareRootTest, PositiveNos) { 
+//           ASSERT_EQ(6, squareRoot(36.0));
+//               ASSERT_EQ(18.0, squareRoot(324.0));
+//       	    ASSERT_EQ(25.4, squareRoot(645.16));
+//       	        ASSERT_EQ(0, squareRoot(0.0));
+//}
+//
+//TEST(SquareRootTest, NegativeNos) {
+//           ASSERT_EQ(-1.0, squareRoot(-15.0));
+//               ASSERT_EQ(-1.0, squareRoot(-0.2));
+//}
+
+TEST(math_tests,add_func_add_two_intigers){
+	Math math;
+	auto result = math.add(2,4);
+	ASSERT_EQ(result,6);
+	
+}
+
+int main(int argc, char **argv)
+{
+	    testing::InitGoogleTest(&argc, argv);
+	        return RUN_ALL_TESTS();
+}
