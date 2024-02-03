5851f1ef2e26ee580cbb5d2d0b83046bbdf0f8f4
hafeezkhn <55631127+hafeezkhn@users.noreply.github.com>
2024-02-03 13:20:09 +0530

    updated

--- runcoverage.sh
+++ runcoverage.sh
@@ -2,7 +2,19 @@ mkdir build||rm -fr build/*  ;cd build;cmake ..
 make -j8
 ./runTests > /dev/null
 gcov -b runTests --object-file CMakeFiles/runTests.dir/main.cpp.gcno
-lcov -c -d .. -o my.info
-genhtml -o results/ my.info
-firefox results/index.html
+python3.8 --version
+if [ $? -eq 0 ]
+then
+	echo "generating gcovr reports...\n"
+	sleep 5
+	python -m gcovr -g -r ..  --html-nested coverage.html
+	mkdir Results||rm -rf Results && mv cover* ./Results
+	firefox Results/coverage.html
+else
+	echo "python>=3.8 not installed\n...generating lcov reports"
+	sleep 5
+	lcov -c -d .. -o my.info
+	genhtml -o results/ my.info
+	firefox results/index.html
+fi
 
